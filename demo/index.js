f61141b462b82e39b848a464a1b124e50965580a
Gavyn McKenzie <gavyn@etchapps.com>
2018-04-10 14:01:25 +0100

    feat: Write a usable demo

--- demo/index.js
+++ demo/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import FlexGrid from '../FlexGrid';
 import './main.scss';
 
 ReactDOM.render(
-  <div>Hello world</div>,
+  <div>
+    <h1>Flex grid example</h1>
+    <FlexGrid>
+      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(i => (
+        <FlexGrid.Item grow key={i}>Item</FlexGrid.Item>
+      ))}
+    </FlexGrid>
+  </div>,
   document.getElementById('app'),
 );
