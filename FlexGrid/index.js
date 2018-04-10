845504adaa64058455e4c9ecb8913a2af3768f5e
Gavyn McKenzie <gavyn@etchapps.com>
2018-04-10 12:13:38 +0100

    feat: Add the files and a readme

---
+++ FlexGrid/index.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+function Item({ children, grow, shrink, align, xs, sm, md, lg, flex, className, ...props }) {
+  const cx = classNames({
+    'flex-grid__item': true,
+    'flex-grid__item--grow': grow,
+    'flex-grid__item--shrink': shrink,
+    'flex-grid__item--flex': flex,
+    [`flex-grid__item--align-${align}`]: align !== '',
+    [`flex-grid__item--xs-${xs}`]: xs,
+    [`flex-grid__item--sm-${sm}`]: sm,
+    [`flex-grid__item--md-${md}`]: md,
+    [`flex-grid__item--lg-${lg}`]: lg,
+    [`${className}`]: className !== '',
+  });
+
+  return (
+    <div {...props} className={cx}>
+      {children}
+    </div>
+  );
+}
+
+Item.propTypes = {
+  children: PropTypes.node,
+  grow: PropTypes.bool,
+  shrink: PropTypes.bool,
+  flex: PropTypes.bool,
+  align: PropTypes.string,
+  xs: PropTypes.number,
+  sm: PropTypes.number,
+  md: PropTypes.number,
+  lg: PropTypes.number,
+  className: PropTypes.string,
+};
+
+Item.defaultProps = {
+  children: '',
+  flex: false,
+  grow: false,
+  shrink: false,
+  align: '',
+  xs: 0,
+  sm: 0,
+  md: 0,
+  lg: 0,
+  className: '',
+};
+
+function Grid({ children, direction, align, justify, wrap, className, ...props }) {
+  const cx = classNames({
+    'flex-grid': true,
+    [`flex-grid--${direction}`]: direction !== '',
+    [`flex-grid--align-${align}`]: align !== '',
+    [`flex-grid--justify-${justify}`]: justify !== '',
+    'flex-grid--wrap': wrap,
+    [`${className}`]: className !== '',
+  });
+
+  return (
+    <div {...props} className={cx}>
+      {children}
+    </div>
+  );
+}
+
+Grid.propTypes = {
+  children: PropTypes.node,
+  align: PropTypes.string,
+  wrap: PropTypes.bool,
+  direction: PropTypes.string,
+  justify: PropTypes.string,
+  className: PropTypes.string,
+};
+
+Grid.defaultProps = {
+  children: '',
+  align: '',
+  justify: '',
+  wrap: false,
+  direction: '',
+  className: '',
+};
+
+Grid.Item = Item;
+
+export default Grid;
---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Flex Grid
+
+A super simple, flexbox based, grid system.
